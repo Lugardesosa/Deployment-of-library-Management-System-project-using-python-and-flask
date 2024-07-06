6c026b266157c55a0b11f4a6fbfd6a632fb16580
Lugard Esosa <talk2lugard@gmail.com>
2024-07-06 19:14:18 +0100

    updated file

--- App/Actor.py
+++ App/Actor.py
@@ -37,7 +37,7 @@ class Actor():
 		def decorated_function(*args, **kwargs):
 			if self.sess_key in session and session[self.sess_key] is not None:
 			    return redirect(self.route_url+path)
-			return f(*args, **kwargs)
+			 return f(*args, **kwargs)
 		return decorated_function
 
 	def signout(self):
